a440eab2c670b8542130ce75c0a036e2cde7ba89
xuanan-zhu <1063785553@qq.com>
2023-07-02 12:52:02 +0800

    fix screen show

--- js/a.js
+++ js/a.js
@@ -2,7 +2,7 @@ var num = -1;
 function changePic() {
         num = ((num+1)%7)+1;
         var ps = document.getElementById("photoshow");
-        ps.src = "./img/pic"+num+".jpg"
+        ps.src = "./img/pic"+num+".jpg";
 }
 function toggleSound() {
     var music = document.getElementById("music");//获取ID
@@ -12,7 +12,7 @@ function toggleSound() {
     music.paused = false;
     music.play();
 }
-var wp = document.getElementById("right_write");
+var wp = document.getElementById("left_write");
 wp.onclick = function () {
     var w_img = document.getElementById("writeshow");
     w_img.src = "./img/lettershow.jpg";
